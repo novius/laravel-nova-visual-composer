984bfac4ff3240519bdd443dc9936bc626f249ca
Tony BOTALLA <botalla@novius.fr>
2020-02-06 08:24:25 +0100

    Refacto positions to prevent draggable bugfix

--- resources/js/components/DetailField.vue
+++ resources/js/components/DetailField.vue
@@ -5,44 +5,44 @@
 </template>
 
 <script>
-  export default {
-    props: ['resource', 'resourceName', 'resourceId', 'field'],
+    export default {
+        props: ['resource', 'resourceName', 'resourceId', 'field'],
 
-    data: () => ({
-      template: 'Loading ...',
-    }),
+        data: () => ({
+            template: 'Loading ...',
+        }),
 
-    mounted() {
-      if (!this.field.value) {
-        this.template = '-';
-        return;
-      }
+        mounted() {
+            if (!this.field.value) {
+                this.template = '-';
+                return;
+            }
 
-      let rowsParam = JSON.parse(this.field.value);
-      rowsParam.forEach((row, index) => {
-        rowsParam[index] = {
-          template: row['template'],
-          content: '', // Prevent "entity too large" HTTP error
-        };
-      });
+            let rowsParam = JSON.parse(this.field.value);
+            rowsParam.forEach((row, index) => {
+                rowsParam[index] = {
+                    template: row['template'],
+                    content: '', // Prevent "entity too large" HTTP error
+                };
+            });
 
-      Nova.request({
-        url: '/nova-vendor/nova-visual-composer/rows-summary',
-        method: 'GET',
-        params: {
-          rows: JSON.stringify(rowsParam),
+            Nova.request({
+                url: '/nova-vendor/nova-visual-composer/rows-summary',
+                method: 'GET',
+                params: {
+                    rows: JSON.stringify(rowsParam),
+                },
+            }).then(({ data }) => {
+                if (data.error) {
+                    this.$toasted.show(data.message, { type: 'error' });
+                } else {
+                    if (!data.summaryHTML) {
+                        this.template = '-';
+                    } else {
+                        this.template = data.summaryHTML;
+                    }
+                }
+            });
         },
-      }).then(({data}) => {
-        if (data.error) {
-          this.$toasted.show(data.message, {type: 'error'});
-        } else {
-          if (!data.summaryHTML) {
-            this.template = '-';
-          } else {
-            this.template = data.summaryHTML;
-          }
-        }
-      });
-    },
-  }
+    }
 </script>
